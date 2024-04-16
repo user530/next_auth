288c510d583d734e87c35a8d657310559c8cf287
user530 <m.timarsuev@gmail.com>
2024-04-16 23:29:41 +0300

    Profile page placeholder

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function ProfilePage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <h1 className="text-4xl mb-5">Profile</h1>
+            <p className="text-2xl">ProfileData</p>
+        </div>
+    )
+}
