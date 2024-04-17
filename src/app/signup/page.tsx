dfcbcf3c1e7866cf60dd4a2181db62d3799181b0
user530 <m.timarsuev@gmail.com>
2024-04-17 23:58:26 +0300

    Updated signup page to handle api requests

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,65 +3,109 @@ import Link from 'next/link';
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 export default function SignupPage() {
+    const router = useRouter();
+
     const [user, setUser] = React.useState({
         email: '',
         password: '',
         name: '',
     });
 
+    const [btnDisabled, setBtnDisabled] = React.useState(true);
+    const [isLoading, setIsLoading] = React.useState(false);
+
     const onSignup = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.log('submit!');
+
+        try {
+            setIsLoading(true);
+            const res = await axios.post('/api/users/signup', user);
+
+            console.log('Response:');
+            console.log(res);
+
+            router.push('/login')
+
+        } catch (error) {
+            toast.error((error as Error).message ?? 'Something went wrong during signup! Try again later.');
+        } finally {
+            setIsLoading(false);
+        }
     }
+
+    React.useEffect(
+        () => {
+            if (
+                user.email.length > 0
+                && user.name.length > 0
+                && user.password.length > 0
+            )
+                setBtnDisabled(false);
+            else
+                setBtnDisabled(true);
+        },
+        [user]
+    )
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-2xl">Signup Page</h1>
+            {
+                isLoading
+                    ? <p>Loading...</p>
+                    : <>
+                        <h1 className="text-2xl">Signup Page</h1>
 
-            <form className="flex flex-col justify-center p-4" onSubmit={onSignup}>
-                <div className="flex">
-                    <label className="p-2 grow" htmlFor="email">Email</label>
-                    <input
-                        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-                        id="email"
-                        type="text"
-                        value={user["email"]}
-                        onChange={(e) => setUser({ ...user, email: e.target.value })}
-                        placeholder="Email"
-                    />
-                </div>
+                        <form className="flex flex-col justify-center p-4" onSubmit={onSignup}>
+                            <div className="flex">
+                                <label className="p-2 grow" htmlFor="email">Email</label>
+                                <input
+                                    className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                                    id="email"
+                                    type="text"
+                                    value={user["email"]}
+                                    onChange={(e) => setUser({ ...user, email: e.target.value })}
+                                    placeholder="Email"
+                                />
+                            </div>
 
-                <div className="flex">
-                    <label className="p-2 grow" htmlFor="password">Password</label>
-                    <input
-                        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-                        id="password"
-                        type="password"
-                        value={user["password"]}
-                        onChange={(e) => setUser({ ...user, password: e.target.value })}
-                        placeholder="password"
-                    />
-                </div>
+                            <div className="flex">
+                                <label className="p-2 grow" htmlFor="password">Password</label>
+                                <input
+                                    className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                                    id="password"
+                                    type="password"
+                                    value={user["password"]}
+                                    onChange={(e) => setUser({ ...user, password: e.target.value })}
+                                    placeholder="password"
+                                />
+                            </div>
 
 
-                <div className="flex">
-                    <label className="p-2 grow" htmlFor="name">Name</label>
-                    <input
-                        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-                        id="name"
-                        type="text"
-                        value={user["name"]}
-                        onChange={(e) => setUser({ ...user, name: e.target.value })}
-                        placeholder="Name"
-                    />
-                </div>
+                            <div className="flex">
+                                <label className="p-2 grow" htmlFor="name">Name</label>
+                                <input
+                                    className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                                    id="name"
+                                    type="text"
+                                    value={user["name"]}
+                                    onChange={(e) => setUser({ ...user, name: e.target.value })}
+                                    placeholder="Name"
+                                />
+                            </div>
 
-                <button type="submit" className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Signup</button>
+                            <button
+                                type="submit"
+                                className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+                                disabled={btnDisabled}
+                            >Signup</button>
 
-                <Link href={"/login"} className="text-center underline text-sm">To the login page</Link>
-            </form>
+                            <Link href={"/login"} className="text-center underline text-sm">To the login page</Link>
+                        </form>
+                    </>
+            }
         </div>
     )
 }
