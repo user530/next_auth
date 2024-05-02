020b57b961c76c24413b57c0dc5594cfe7ab5975
user530 <m.timarsuev@gmail.com>
2024-05-02 23:24:37 +0300

    Small clean up

--- src/app/api/users/reset_email/route.ts
+++ src/app/api/users/reset_email/route.ts
@@ -1,4 +1,3 @@
-
 import { connect } from '@/dbConfig/dbConfig';
 import { User } from '@/models/userModel';
 import { NextRequest, NextResponse } from 'next/server';
--- src/middleware.ts
+++ src/middleware.ts
@@ -8,13 +8,12 @@ export async function middleware(request: NextRequest) {
     const token = request.cookies.get('token')?.value || '';
 
     // If user already logged in -> no need to login or signup
-    if (isPublic && token)
-        return NextResponse.redirect(new URL('/', request.nextUrl));
+    if (isPublic)
+        return NextResponse.next();
 
     // Private path for unauthenticated user
     if (!isPublic && !token)
         return NextResponse.redirect(new URL('/login', request.nextUrl));
-
 };
 
 export const config = {
